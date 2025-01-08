51264d59dce6e4fd56dff5002872c2df777f23b8
Caio Vellani <caiovellani@outlook.com>
2025-01-08 19:34:18 -0300

    react server components

--- src/app/catalog/[...data]/page.tsx
+++ src/app/catalog/[...data]/page.tsx
@@ -1,16 +1,32 @@
+"use client";
+
 type ProductProps = {
 	params: {
 		data: string[];
 	};
 };
 
+// Sever Components -> Não USA JavaScript no lado do cliente
+// Client Components -> O JavaScript é enviado ao navegador (cliente)
+
 export default function Product({ params }: ProductProps) {
 	const [productId, size, color] = params.data;
+
+	console.log(params);
+
+	function addToCart() {
+		console.log("Adicionou ao carrinho");
+	}
+
 	return (
 		<div>
 			<p>Product: {productId}</p>
 			<p>Size: {size}</p>
 			<p>Color: {color}</p>
+
+			<button type="button" onClick={addToCart}>
+				Adicionar ao carrinho
+			</button>
 		</div>
 	);
 }
